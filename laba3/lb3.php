45954e4de5c9628afadd279207389cd3377517fd
thatonew <137867552+thatonew@users.noreply.github.com>
2024-09-30 17:07:46 +0700

    Add files via upload

---
+++ laba3/lb3.php
@@ -0,0 +1,205 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>69 Media</title>
+    <link rel="icon" type="image/x-icon" href="https://i.imgur.com/u0oYgyi.png">
+    <meta
+      property="og:url"
+      content="C:\Users\mkyub\OneDrive\Рабочий стол\loh\error.php"
+    />
+    <meta property="og:title" content="T&amp;P Drops" />
+    <meta property="og:description" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content=" " />
+    <link rel="canonical" href="https://vk.com/feed" />
+    <!--/metatextblock-->
+    <meta name="format-detection" content="telephone=no" />
+    <meta http-equiv="x-dns-prefetch-control" content="on" />
+    <link rel="dns-prefetch" href=" " />
+    <link rel="dns-prefetch" href="https://vk.com/feed" />
+    <meta name="robots" content="nofollow" />
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
+
+  </head>
+  <body id="stars-container" class="t-body" style="margin: 0px;">
+
+      <div id='stars'></div>
+      <div id='stars2'></div>
+      <div id='stars3'></div>
+
+    <!--allrecords-->
+
+
+
+    <div
+      id="allrecords"
+      class="t-records"
+      data-hook="blocks-collection-content-node"
+      data-project-id="4793505"
+      data-page-id="23634064"
+      data-page-alias="drops"
+      data-formskey="65ec072bbd43af88a52831934ae5312a"
+      data-cookie="no"
+      data-lazy="yes"
+      data-project-lang="EN"
+      data-root-zone="com"
+    >
+      <div
+        id="rec381512573"
+        class="r t-rec"
+        style="box-shadow: 0 2px 30px rgb(0 255 245); margin-top:-5px ; "
+        data-animationappear="off"
+        data-record-type="456"
+      >
+        <!-- T456 -->
+        <div id="nav381512573marker"></div>
+        <div
+          id="nav381512573"
+          class="t456 t456__positionstatic"
+          style="background-color: rgb(0 0 0 / 50%); "
+          data-bgcolor-hex="#000000"
+          data-bgcolor-rgba="rgba(255,0,0,0.5)"
+          data-navmarker="nav381512573marker"
+          data-appearoffset=""
+          data-bgopacity-two=""
+          data-menushadow=""
+          data-menushadow-css=""
+          data-bgopacity="1"
+          data-menu-items-align="right"
+          data-menu="yes"
+        >
+          <div class="t456__maincontainer" style="">
+            <div class="t456__leftwrapper" style="min-width:90px;width:90px;">
+              <div class="t456__logowrapper" style="display: block;">
+                <a onclick="document.location='index.php'">
+                  <img
+                    class="t456__imglogo t456__imglogomobile"
+                    src="https://i.imgur.com/kgkcVti.png"
+                    imgfield="img"
+                    style="max-width: 90px; width: 90px;"
+                    alt="Company"
+                  />
+                </a>
+              </div>
+            </div>
+            <nav class="t456__rightwrapper t456__menualign_right" style="">
+              <ul role="list" class="t456__list t-menu__list">
+                <li class="t456__list_item" style="padding:0 15px;">
+
+                </li>
+                <li class="t456__list_item" style="padding:0 0 0 15px;">
+
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div
+        id="rec381516049"
+        class="r t-rec t-rec_pt_90 t-rec_pb_135"
+        style="padding-top:90px;padding-bottom:135px;background-color: rgba(255, 0, 0, 0); "
+        data-record-type="468"
+        data-bg-color="#000000"
+        data-animationappear="off">
+
+
+
+        <!-- t468 -->
+        
+        <style>
+          #rec381516049 .t468__uptitle { text-transform: uppercase; }
+        </style>
+      </div>
+      <div
+        id="rec381555438"
+        class="r t-rec t-rec_pt_0 t-rec_pb_0"
+        style="padding-top:0px;padding-bottom:0px; "
+        data-animationappear="off"
+        data-record-type="131"
+      >
+        <!-- T123 -->
+        <div class="t123">
+          <div class="t-container_100">
+            <div class="t-width t-width_100">
+              <link
+                href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <title>Login Form</title>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="form-box">
+            <div class="form-value">
+                <form action="calculate.php" method="post">
+                    <h2>Задачи на PHP</h2>
+                    <div class="inputbox">
+                        <input type="text" name="A" required>
+                        <label for="">Введите число А:</label>
+                    </div>
+                    <div class="inputbox">
+                        <input type="text" name="B" required>
+                        <label for="">Введите число Б:</label>
+                    </div>
+                    <div class="inputbox">
+                        <input type="text" name="N" required>
+                        <label for="">Введите значение N:</label>
+                      
+                    </div>
+                    
+                    <button type="submit">Рассчитать</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="result">
+
+      <div
+        id="rec381529221"
+        class="r t-rec t-rec_pt_15 t-rec_pb_60"
+        style="margin-top: 68px;padding-top:15px;background-color: rgba(0, 0, 0, 0.5); "
+        data-record-type="463"
+        data-bg-color="#000000"
+        data-animationappear="off"
+      >
+        <!-- T463 -->
+        <div class="t463" id="t-footer_381529221">
+          <div class="t463__maincontainer">
+            <div class="t463__content" style="">
+              <div class="t463__colwrapper">
+                <div class="t463__col">
+                  <a class="t463__link" href="/"
+                    ><div class="t463__logo t-title" field="title"></div
+                  ></a>
+                </div>
+                <div class="t463__col t463__col_center t-align_center">
+                <div
+                    class="t463__typo t463__copyright t-name t-name_xs"
+                    field="text"
+                  >
+                    © 2024 by ASTRAL DESIGN
+                  </div>
+                </div>
+                <div class="t463__col t-align_right"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
